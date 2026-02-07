a3f80d9ed51d327aa7eec4ed5e08784ab2e9218d
gaby28894178 <28894178+gaby28894178@users.noreply.github.com>
2026-02-07 18:23:44 -0300

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
+import MobileMenu from "./components/MobileMenu";
 import { Routes, Route } from "react-router-dom";
 import Shop from "./pages/Shop";
 import Aroma from "./pages/Aroma";
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <MobileMenu />
       <main style={{ padding: 20, marginTop: 70, minHeight: 'calc(100vh - 70px)' }}>
         <Routes>
           <Route path="/" element={<Shop />} />
